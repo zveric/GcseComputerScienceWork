cdeef58c24f073999d39dac9c92e7daafa8754b3
Pastabikes <74837915+zveric@users.noreply.github.com>
2021-11-03 11:00:26 +0000

    Did more to hangman (Still no where near done!!)

---
+++ Programming/asciitree.py
@@ -0,0 +1,4 @@
+print('ascii tree')
+
+lives = 5 
+
--- Programming/hangman.py
+++ Programming/hangman.py
@@ -1,7 +1,44 @@
 import random;
+
+def Guessing(word):
+  guess = input("Guess a letter:")
+  word_there=(word.find(guess))
+  print (word)
+ 
+  if word_there <0:
+    print("That letter isnt present")
+  else:
+    print ("Well done you guessed correctly you guessed letter no:", word_there+1)
+    Guessing()
+
+
+
+  
+
+  
+
+
+
+
+
+
+
 print("Welcome to Hangman")
 words = ["comfortable", "homeless", "jobless", "juggle", "spare", "shade", "fearless", "extra-small", "guitar", "sneaky", "structure", "equable", "voiceless", "mark, size", "furry", "cut", "mass", "psychotic", "savory", "station", "yam", "bead", "thin", "poke", "weather", "jolly", "foamy", "needle", "exotic", "seal", "bent", "immense", "exercise", "ordinary", "laughable" ,"pause", "record", "tiny", "burly", "lock"]
   
 aim_word = (random.choice(words))
 chars = len(aim_word)
 print ("word length is", chars)
+
+Guessing(aim_word)
+
+
+
+
+
+
+
+
+
+
+
