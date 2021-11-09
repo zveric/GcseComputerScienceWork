22f8ca5c1b8c3bb4f2acaa829c6571517f53a3be
Eric <74837915+zveric@users.noreply.github.com>
2021-11-09 14:37:51 +0000

    Hangman now works

--- Programming/Hangman/hangman.py
+++ Programming/Hangman/hangman.py
@@ -1,18 +1,6 @@
 import random;
 
-# Function to run the guessing code
-def Guessing(word,wordleft):
-  guess = input("Guess a letter:")
-  letno=(word.find(guess))
-  print (word)
-  int (wordleft)
- 
-  if letno <0:
-    print("That letter isnt present")
-  else:
-    print ("Well done you guessed correctly you guessed letter no:", letno+1)
-    wordleft = wordleft-1
-    return wordleft
+
 
 # Setup
 
@@ -20,13 +8,13 @@ trys = 0
 int (trys)
 
 # List of words
-words = ["comfortable", "homeless", "jobless", "juggle", "spare", "shade", "fearless", "extra-small", "guitar", "sneaky", "structure", "equable", "voiceless", "mark, size", "furry", "cut", "mass", "psychotic", "savory", "station", "yam", "bead", "thin", "poke", "weather", "jolly", "foamy", "needle", "exotic", "seal", "bent", "immense", "exercise", "ordinary", "laughable" ,"pause", "record", "tiny", "burly", "lock"]
+words = ["comfortable", "homeless", "jobless", "juggle", "spare", "shade", "fearless", "extra-small", "guitar", "sneaky", "structure", "equable", "voiceless", "mark", "size", "furry", "cut", "mass", "psychotic", "savory", "station", "yam", "bead", "thin", "poke", "weather", "jolly", "foamy", "needle", "exotic", "seal", "bent", "immense", "exercise", "ordinary", "laughable" ,"pause", "record", "tiny", "burly", "lock"]
 
 
 aim_word = (random.choice(words))
 wordlen = len(aim_word)
 int(wordlen)
-wordlen = wordlen + 1
+wordlen = wordlen 
 wordleft = wordlen
   
 # Program seen by user
@@ -34,15 +22,18 @@ wordleft = wordlen
 print("Welcome to Hangman")
 print ("The word is", wordlen, "letters long")
 
-Guessing(aim_word,wordleft)
+while wordleft > 0:
+  guess = input("Guess a letter:")
+  letno=(aim_word.find(guess))
 
-while  <= wordlen:
-  Guessing(aim_word,wordleft) 
-  trys = trys + 1
-
-
-
-
+  int (wordleft)
+ 
+  if letno <0:
+    print("That letter isnt present")
+  elif letno !<0:
+    print ("Well done you guessed correctly you guessed letter no:", letno+1)
+    wordleft = wordleft-1
+    
 
 
 
