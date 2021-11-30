c31b6b9715fbb6d6552d1b732eaca16a2001368f
Davecastle10 <91599399+Davecastle10@users.noreply.github.com>
2021-11-30 19:30:12 +0000

    annoying code that doesnt work

---
+++ Programming/Algorithms/Searching/linear.py
@@ -0,0 +1,29 @@
+#https://www.geeksforgeeks.org/python-program-for-linear-search/
+
+# Searching an element in a list/array in python
+# can be simply done using \'in\' operator
+# Example:
+# if x in arr:
+# print arr.index(x)
+
+# If you want to implement Linear Search in python
+
+# Linearly search x in arr[]
+# If x is present then return its location
+# else return -1
+
+def search(arr, x):
+ 
+	for i in range(len(arr)):
+    if arr[i] == x:
+      return i
+
+	return -1
+
+arr = [ 2, 3, 4, 10, 40 ]
+x = 4
+search(arr)
+
+print("FUCK THIS SHIT I'M OUT!!!")
+#this code is bastard
+
--- Programming/Algorithms/Sorting/insertion_sort.py
+++ Programming/Algorithms/Sorting/insertion_sort.py
@@ -5,10 +5,10 @@ def insertionSort(arr):
       #than key,
       # to one position ahead of their current position
       j = i-1 # j is the seccond to last item (i think)
-      while j >=0 and key < arr[j] :
-         arr[j+1] = arr[j]
-         j -= 1
-      arr[j+1] = key
+      while j >=0 and key < arr[j] :# while j is not below 0  and key is less than j, keeps looping until sorted
+         arr[j+1] = arr[j]# position above j becomes j
+         j -= 1# j moves down a position
+      arr[j+1] = key# key is the positionin front of j
 # main
 arr = ['t','u','t','o','r','i','a','l']
 insertionSort(arr)
