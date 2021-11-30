21ec06679bf2ecb062a6c7b1823b7cad4a9aff72
Davecastle10 <91599399+Davecastle10@users.noreply.github.com>
2021-11-30 17:46:04 +0000

    added comments

--- Programming/Daves_random_stuff/Numpy_search_sort.py
+++ Programming/Daves_random_stuff/Numpy_search_sort.py
@@ -1,6 +1,6 @@
 # this code returns all index postitions
 # where 4 is, which is shown by printin x
-
+print("first1")
 import numpy as np
 arr = np.array([1, 2, 3, 4, 5, 4, 4])
 x = np.where(arr == 4)
@@ -10,7 +10,7 @@ print(x)
 
 #this code returns index postitions
 #where all values are even
-
+print("first2")
 import numpy as np
 arr = np.array([1, 2, 3, 4, 5, 6, 7, 8])
 x = np.where(arr%2 == 0)
@@ -20,7 +20,7 @@ print(x)
 
 #this code is werid
 #it requires a sorted list
-
+print("first3")
 import numpy as np
 arr = np.array([6, 7, 8, 9,])
 x = np.searchsorted(arr, 8)
@@ -29,7 +29,7 @@ print(x)
 #Sorts list by default, e.g alphabeticall, 
 #or by number for each digit, so 11 goes before 3 
 # becasue 1 is less than 3 (1<3)
-
+print("first4")
 cars = ['Ford', 'BMW', 'Volvo', 'Aston', '11', '3']
 cars.sort()
 print(cars)
